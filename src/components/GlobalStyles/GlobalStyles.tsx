298781f3e3db87fd82b74fb311fc3e7719d28a43
The Tien <thetien9a555@gmail.com>
2024-03-05 09:39:38 +0700

    first commit

---
+++ src/components/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './_normalize.scss';
+import './GlobalStyles.scss';
+const GlobalStyles = ({ children }: { children: React.ReactNode }) => {
+    return children;
+};
+
+export default GlobalStyles;
